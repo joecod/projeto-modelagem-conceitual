59a119269c974a9837bfe24c69d37b9466fc113c
Jose Lemes <jose.lemes7@gmail.com>
2021-10-05 19:37:38 -0300

    include h2

--- src/main/java/com/joselemes/cursomc/domain/Categoria.java
+++ src/main/java/com/joselemes/cursomc/domain/Categoria.java
@@ -2,6 +2,11 @@ package com.joselemes.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.*;
 
 @Data
@@ -9,9 +14,12 @@ import lombok.*;
 @NoArgsConstructor
 @EqualsAndHashCode
 @ToString
+@Entity
 public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 
